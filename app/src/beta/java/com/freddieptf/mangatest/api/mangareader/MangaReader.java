bf1f336c9f2064f41153b5e8402576e0b6fb632c
freddieptf <freddmuiru@gmail.com>
2016-04-23 21:59:13 +0300

    Begone JSOUP DOCUMENTS MESS. Use new mangareader api

--- app/src/beta/java/com/freddieptf/mangatest/api/mangareader/MangaReader.java
+++ app/src/beta/java/com/freddieptf/mangatest/api/mangareader/MangaReader.java
@@ -2,79 +2,54 @@ package com.freddieptf.mangatest.api.mangareader;
 
 import com.freddieptf.mangatest.api.ApiUtils;
 import com.freddieptf.mangatest.api.helperInterfaces.GetListListener;
-import com.freddieptf.mangatest.api.helperInterfaces.OnDocumentReceived;
-
-import org.jsoup.nodes.Document;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
 
 /**
- * Created by fred on 7/25/15.
- * MangaReader Class:
- *
- * Class providing methods that process the Jsoup documents
- * and return Lists with objects containing manga list data.
+ * Created by fred on 7/25/15..
  */
 public class MangaReader {
 
     final String LOG_TAG = getClass().getSimpleName();
 
-    GetDocuments getDocuments;
-
+    Processor processor;
     public MangaReader(){
-        getDocuments = new GetDocuments();
+        processor = new Processor();
     }
 
-//    public void getMangaList(final GetListListener listener){
-//        getDocuments.getAlphabeticalListDocument(new OnDocumentReceived() {
-//            @Override
-//            public void onDocumentReceived(Document document) {
-//                listener.onGetList(Processor.processAlphabeticalListDocument(document));
-//            }
-//        });
-//    }
-
     public void getMangaList(final GetListListener listener){
-
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    URL baseUrl = new URL("https://doodle-manga-scraper.p.mashape.com/mangareader.net/");
+                    URL baseUrl = new URL("http://mapi-freddieptf.rhcloud.com/api/mr/list");
                     String result = ApiUtils.getResultString(baseUrl);
-                    listener.onGetList(new com.freddieptf.mangatest.api.mangafox.Processor().processList(result));
+                    listener.onGetList(processor.processMangaListJSON(result));
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
                 }
-
             }
         }).start();
-
-    }
-
-    public void getPopularList(final GetListListener listener){
-        getDocuments.getPopularListDocument(new OnDocumentReceived() {
-            @Override
-            public void onDocumentReceived(List<Document> document) {
-                listener.onGetList(Processor.processPopularListDocument(document));
-            }
-        });
     }
 
     public void getLatestList(final GetListListener listener){
-        getDocuments.getLatestListDocument(new OnDocumentReceived() {
+        new Thread(new Runnable() {
             @Override
-            public void onDocumentReceived(List<Document> document) {
-                listener.onGetList(Processor.processLatestListDocument(document.get(0)));
+            public void run() {
+                try {
+                    URL baseUrl = new URL("http://mapi-freddieptf.rhcloud.com/api/mr/latest");
+                    String result = ApiUtils.getResultString(baseUrl);
+                    listener.onGetList(processor.processLatestListJSON(result));
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
             }
-        });
+        }).start();
     }
 
-
-
-
+    public void getPopularList(final GetListListener listener){
+    }
 
 
 }
--- app/src/beta/java/com/freddieptf/mangatest/api/mangareader/Processor.java
+++ app/src/beta/java/com/freddieptf/mangatest/api/mangareader/Processor.java
@@ -3,10 +3,12 @@ package com.freddieptf.mangatest.api.mangareader;
 import com.freddieptf.mangatest.beans.MangaInfoBean;
 import com.freddieptf.mangatest.beans.MangaLatestInfoBean;
 import com.freddieptf.mangatest.beans.MangaPopularInfoBean;
+import com.freddieptf.mangatest.utils.Utilities;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,70 +18,53 @@ import java.util.List;
  */
 public class Processor {
 
-    private static final String UL_TAG_CLASS = "series_alpha";
-    private static final String TR_TAG_CLASS = "c2";
+    private static final String MANGA_ID = "id";
+    private static final String MANGA_NAME = "manga";
+    private static final String MANGA_CHAPTER = "chapter";
+    private static final String MANGA_RELEASE_DATE = "release_date";
 
-    public static List<MangaInfoBean> processAlphabeticalListDocument(Document document) {
-        Elements li_mangaList = document.select("ul." + UL_TAG_CLASS + " > li");
+    public List<MangaInfoBean> processMangaListJSON(String result) {
         List<MangaInfoBean> list = new ArrayList<>();
-        Element e;
-        for (Element element : li_mangaList) {
-            MangaInfoBean mangaInfoBean = new MangaInfoBean();
-            e = element.child(0);
-            mangaInfoBean.setManga_ID(e.attr("href").replace("/", ""));
-            mangaInfoBean.setManga_NAME(e.text());
-            list.add(mangaInfoBean);
-        }
-
-        return list;
-    }
-
-    public static List<MangaLatestInfoBean> processLatestListDocument(Document document) {
-        Elements tr_LatestElement = document.select("tr." + TR_TAG_CLASS);
-
-        List<MangaLatestInfoBean> list = new ArrayList<>();
-
-        for(Element element : tr_LatestElement){
-            MangaLatestInfoBean latestInfoBean = new MangaLatestInfoBean();
-            String date = element.select("td.c1").text();
-            String mangaId = element.select("a.chapter").attr("href").replace("/", "");
-            String mangaName = element.select("a.chapter").text();
-            Elements el = element.getElementsByTag("a");
-            String chapter = el.get(1).text();
-            int ch = Integer.parseInt(chapter.substring(mangaName.length() + 1));
-
-            latestInfoBean.setDate(date);
-            latestInfoBean.setMangaId(mangaId);
-            latestInfoBean.setMangaTitle(mangaName);
-            latestInfoBean.setChapter(ch);
-            list.add(latestInfoBean);
-        }
-
-        return list;
-
-    }
-
-    public static List<MangaPopularInfoBean> processPopularListDocument(List<Document> documents){
-        List<MangaPopularInfoBean> list = new ArrayList<>();
-
-        for(Document document : documents) {
-            Elements div_popularMangas = document.select("div.mangaresultitem");
-
-            for (Element element : div_popularMangas) {
-                MangaPopularInfoBean popularInfoBean = new MangaPopularInfoBean();
-                String name = element.select("h3 > a").text();
-                String author = element.select("div.author_name").text();
-                String chapterCount = element.select("div.chapter_count").text();
-                String genres = element.select("div.manga_genre").text();
-
-                popularInfoBean.setAuthor(author);
-                popularInfoBean.setName(name);
-                popularInfoBean.setChapterCount(chapterCount);
-                popularInfoBean.setGenre(genres);
-                list.add(popularInfoBean);
+        try {
+            JSONArray array = new JSONArray(result);
+            for(int i = 0; i < array.length(); i++){
+                MangaInfoBean mangaInfoBean = new MangaInfoBean();
+                JSONObject object = array.getJSONObject(i);
+                mangaInfoBean.setManga_ID(object.getString(MANGA_ID));
+                mangaInfoBean.setManga_NAME(object.getString(MANGA_NAME));
+                list.add(mangaInfoBean);
             }
+            int i = 0;
+            for(MangaInfoBean m : list){
+                i++;
+                if(i < 10) Utilities.Log("Processsor", m.getManga_NAME());
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+        return list;
+    }
 
+    public List<MangaLatestInfoBean> processLatestListJSON(String result) {
+        List<MangaLatestInfoBean> list = new ArrayList<>();
+        try {
+            JSONArray array = new JSONArray(result);
+            for(int i= 0; i < array.length(); i++){
+                MangaLatestInfoBean latestInfoBean = new MangaLatestInfoBean();
+                JSONObject object = array.getJSONObject(i);
+                latestInfoBean.setDate(object.getString(MANGA_RELEASE_DATE));
+                latestInfoBean.setMangaTitle(object.getString(MANGA_NAME));
+                latestInfoBean.setChapter(Integer.parseInt(object.getString(MANGA_CHAPTER)));
+                list.add(latestInfoBean);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public List<MangaPopularInfoBean> processPopularListJSON(List<Document> documents){
+        List<MangaPopularInfoBean> list = new ArrayList<>();
         return list;
     }
 }
