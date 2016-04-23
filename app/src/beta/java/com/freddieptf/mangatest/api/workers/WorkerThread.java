007b0214f4247f56e26854ecfd856ae7d1e488d1
freddieptf <freddmuiru@gmail.com>
2016-04-23 21:57:44 +0300

    Run the tasks in their own threads

--- app/src/beta/java/com/freddieptf/mangatest/api/workers/WorkerThread.java
+++ app/src/beta/java/com/freddieptf/mangatest/api/workers/WorkerThread.java
@@ -6,16 +6,15 @@ import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 
-import com.freddieptf.mangatest.api.mangafox.MangaFox;
-import com.freddieptf.mangatest.api.mangareader.MangaReader;
 import com.freddieptf.mangatest.api.helperInterfaces.GetListListener;
 import com.freddieptf.mangatest.api.helperInterfaces.InsertListener;
+import com.freddieptf.mangatest.api.mangafox.MangaFox;
+import com.freddieptf.mangatest.api.mangareader.MangaReader;
 import com.freddieptf.mangatest.data.Contract;
 import com.freddieptf.mangatest.utils.Utilities;
 
 import java.util.List;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * Created by fred on 8/24/15.
@@ -42,96 +41,111 @@ public class WorkerThread extends Thread {
         mangaReader = new MangaReader();
         mangaFox = new MangaFox();
         this.context = context;
-        service = Executors.newFixedThreadPool(3);
     }
-
 
     @Override
     public void run() {
         for(int task : tasks) {
             switch (task) {
                 case READER_ALPHA_LIST:
-                    mangaReader.getMangaList(new GetListListener() {
+                    new Thread(new Runnable() {
                         @Override
-                        public void onGetList(@NonNull List list) {
-                            Utilities.Log(LOG_TAG, "Reader Alphabet list: " + list.size());
-                            if(list.size() > 0) {
-                                context.getContentResolver().delete(Contract.MangaReaderMangaList.CONTENT_URI, null, null);
-                                context.getContentResolver().delete(Contract.VirtualTable.CONTENT_URI, null, null);
-                                final InsertCall insertCall = new InsertCall(list, Contract.MangaReaderMangaList.CONTENT_URI, context);
-                                service.submit(insertCall);
-
-                                insertCall.setInsertDoneListener(new InsertListener() {
-                                    @Override
-                                    public void onInsertDone() {
-                                        Utilities.Log(LOG_TAG, "Reader insert done");
+                        public void run() {
+                            mangaReader.getMangaList(new GetListListener() {
+                                @Override
+                                public void onGetList(@NonNull List list) {
+                                    Utilities.Log(LOG_TAG, "Reader Alphabet list: " + list.size());
+                                    if (list.size() > 0) {
+                                        context.getContentResolver().delete(Contract.MangaReaderMangaList.CONTENT_URI, null, null);
+                                        context.getContentResolver().delete(Contract.VirtualTable.CONTENT_URI, null, null);
+                                        final InsertCall insertCall = new InsertCall(list, Contract.MangaReaderMangaList.CONTENT_URI, context);
+                                        insertCall.start();
+                                        insertCall.setInsertDoneListener(new InsertListener() {
+                                            @Override
+                                            public void onInsertDone() {
+                                                Utilities.Log(LOG_TAG, "Reader insert done");
+                                            }
+                                        });
                                     }
-                                });
-                            }
-                        }
-                    });
+                                }
+                            });
 
+                        }
+                    }).start();
                     break;
                 case FOX_ALPHA_LIST:
-                    mangaFox.getMangaList(new GetListListener() {
+                    new Thread(new Runnable() {
                         @Override
-                        public void onGetList(@NonNull List list) {
-                            Utilities.Log(LOG_TAG, "Fox Alphabet: " + list.size());
-                            if (list.size() > 0) {
-                                context.getContentResolver().delete(Contract.MangaFoxMangaList.CONTENT_URI, null, null);
+                        public void run() {
+                            mangaFox.getMangaList(new GetListListener() {
+                                @Override
+                                public void onGetList(@NonNull List list) {
+                                    Utilities.Log(LOG_TAG, "Fox Alphabet: " + list.size());
+                                    if (list.size() > 0) {
+                                        context.getContentResolver().delete(Contract.MangaFoxMangaList.CONTENT_URI, null, null);
 //                                context.getContentResolver().delete(Contract.VirtualTable.CONTENT_URI, null, null);
 
-                                InsertCall insertCall = new InsertCall(list, Contract.MangaFoxMangaList.CONTENT_URI, context);
-                                service.submit(insertCall);
-
-                                insertCall.setInsertDoneListener(new InsertListener() {
-                                    @Override
-                                    public void onInsertDone() {
-                                        Utilities.Log(LOG_TAG, "Fox insert done");
+                                        InsertCall insertCall = new InsertCall(list, Contract.MangaFoxMangaList.CONTENT_URI, context);
+                                        insertCall.start();
+                                        insertCall.setInsertDoneListener(new InsertListener() {
+                                            @Override
+                                            public void onInsertDone() {
+                                                Utilities.Log(LOG_TAG, "Fox insert done");
+                                            }
+                                        });
                                     }
-                                });
-                            }
+                                }
+                            });
                         }
-                    });
+                    }).start();
                     break;
                 case READER_LATEST_LIST:
-                    mangaReader.getLatestList(new GetListListener() {
+                    new Thread(new Runnable() {
                         @Override
-                        public void onGetList(@NonNull List list) {
-                            Utilities.Log(LOG_TAG, "Latest: " + list.size());
-                            if (list.size() > 0) {
-                                context.getContentResolver().delete(Contract.MangaReaderLatestList.CONTENT_URI, null, null);
-                                InsertCall insertCall = new InsertCall(list, Contract.MangaReaderLatestList.CONTENT_URI, context);
-                                service.submit(insertCall);
-
-                                insertCall.setInsertDoneListener(new InsertListener() {
-                                    @Override
-                                    public void onInsertDone() {
-                                        Utilities.Log(LOG_TAG, "Latest list insert done!");
+                        public void run() {
+                            mangaReader.getLatestList(new GetListListener() {
+                                @Override
+                                public void onGetList(@NonNull List list) {
+                                    Utilities.Log(LOG_TAG, "Latest: " + list.size());
+                                    if (list.size() > 0) {
+                                        context.getContentResolver().delete(Contract.MangaReaderLatestList.CONTENT_URI, null, null);
+                                        InsertCall insertCall = new InsertCall(list, Contract.MangaReaderLatestList.CONTENT_URI, context);
+                                        insertCall.start();
+                                        insertCall.setInsertDoneListener(new InsertListener() {
+                                            @Override
+                                            public void onInsertDone() {
+                                                Utilities.Log(LOG_TAG, "Latest list insert done!");
+                                            }
+                                        });
                                     }
-                                });
-                            }
+                                }
+                            });
                         }
-                    });
+                    }).start();
                     break;
                 case READER_POPULAR_LIST:
-                    mangaReader.getPopularList(new GetListListener() {
+                    new Thread(new Runnable() {
                         @Override
-                        public void onGetList(@NonNull List list) {
-                            Utilities.Log(LOG_TAG, "Popular: " + list.size());
-                            if (list.size() > 0) {
-                                context.getContentResolver().delete(Contract.MangaReaderPopularList.CONTENT_URI, null, null);
-                                InsertCall insertCall = new InsertCall(list, Contract.MangaReaderPopularList.CONTENT_URI, context);
-                                service.submit(insertCall);
-                                insertCall.setInsertDoneListener(new InsertListener() {
-                                    @Override
-                                    public void onInsertDone() {
-                                        Utilities.Log(LOG_TAG, "Popular list insert done!");
+                        public void run() {
+                            mangaReader.getPopularList(new GetListListener() {
+                                @Override
+                                public void onGetList(@NonNull List list) {
+                                    Utilities.Log(LOG_TAG, "Popular: " + list.size());
+                                    if (list.size() > 0) {
+                                        context.getContentResolver().delete(Contract.MangaReaderPopularList.CONTENT_URI, null, null);
+                                        InsertCall insertCall = new InsertCall(list, Contract.MangaReaderPopularList.CONTENT_URI, context);
+                                        insertCall.start();
+                                        insertCall.setInsertDoneListener(new InsertListener() {
+                                            @Override
+                                            public void onInsertDone() {
+                                                Utilities.Log(LOG_TAG, "Popular list insert done!");
+                                            }
+                                        });
                                     }
-                                });
-                            }
+                                }
+                            });
                         }
-                    });
+                    }).start();
                     break;
 
                 default:
@@ -141,21 +155,9 @@ public class WorkerThread extends Thread {
 
         }
 
-        //calling service.shutdown here causes the executor service to
-        // shutdown (almost immediately) before anything is submitted to it...wut?
-        shutdown();
+
 
 
     }
 
-    void shutdown(){
-        if(!service.isShutdown()){
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    service.shutdown();
-                }
-            }, 0);
-        }
-    }
 }
