06fcd9d47f7683722dc65cc0903deaf086a735a0
freddieptf <freddmuiru@gmail.com>
2018-12-21 15:51:49 +0300

    replace pager with a recyclerview

--- reader/src/main/java/com/freddieptf/reader/widgets/ReaderSeekbar.kt
+++ reader/src/main/java/com/freddieptf/reader/widgets/ReaderSeekbar.kt
@@ -6,8 +6,9 @@ import android.graphics.drawable.LayerDrawable
 import android.util.AttributeSet
 import android.util.TypedValue
 import androidx.core.content.ContextCompat
-import androidx.viewpager.widget.ViewPager
 import com.freddieptf.reader.R
+import com.freddieptf.reader.pagelist.CustomSnapHelper
+import com.freddieptf.reader.utils.ReadMode
 import com.github.rubensousa.previewseekbar.PreviewSeekBar
 import com.github.rubensousa.previewseekbar.PreviewView
 
@@ -16,60 +17,38 @@ import com.github.rubensousa.previewseekbar.PreviewView
  * Created by freddieptf on 11/28/18.
  */
 class ReaderSeekbar @JvmOverloads constructor(context: Context,
-                                              attrs: AttributeSet? = null) : PreviewSeekBar(context, attrs) {
+                                              attrs: AttributeSet? = null)
+    : PreviewSeekBar(context, attrs), CustomSnapHelper.OnPageChangeListener {
 
-    private val pagerPageChangeListener: ViewPager.SimpleOnPageChangeListener
-    private val readDirectionChangeListener: ReaderViewPager.SimpleReadSignals
-    private var direction: ReaderViewPager.DIRECTION? = null
+    private var direction: ReadMode? = null
 
     init {
+    }
 
-        pagerPageChangeListener = object : ViewPager.SimpleOnPageChangeListener() {
-            override fun onPageSelected(position: Int) {
-                progress = position
-            }
-        }
+    fun setUp(direction: ReadMode, progress: Int, max: Int) {
+        this.direction = direction
+        setSeekerProgressDrawable(direction)
+        this.max = max
+        this.progress = progress
+    }
 
-        readDirectionChangeListener = object : ReaderViewPager.SimpleReadSignals() {
-            override fun onReadDirectionChange(direction: ReaderViewPager.DIRECTION) {
-                this@ReaderSeekbar.direction = direction
-                setSeekerProgressDrawable(direction)
-            }
-        }
+    override fun onPageChange(position: Int) {
+        progress = position
+    }
 
+    fun setOnSeekListener(onSeekListener: OnSeekListener?) {
+        addOnPreviewChangeListener(SimplePreviewChangeListener(onSeekListener))
     }
 
     // couldn't override getProgress..it messes up the preview seekbar
-    private fun getRealProgress(): Int {
-        return if (direction == ReaderViewPager.DIRECTION.LEFT_TO_RIGHT) max - getProgress()
+    fun getRealProgress(): Int {
+        return if (direction == ReadMode.LEFT_TO_RIGHT) max - getProgress()
         else getProgress()
     }
 
-    fun setUpWithPager(pager: ReaderViewPager) {
-        if (pager.adapter == null) throw IllegalStateException("cannot setup with pager that has no adapter")
-
-        direction = pager.direction!!
-        setSeekerProgressDrawable(direction!!)
-        max = pager.adapter!!.count - 1
-        progress = pager.currentItem
-
-        pager.addOnPageChangeListener(pagerPageChangeListener)
-        pager.addReadSignalCallback(readDirectionChangeListener)
-
-        addOnPreviewChangeListener(object : PreviewView.OnPreviewChangeListener {
-            override fun onPreview(previewView: PreviewView?, progress: Int, fromUser: Boolean) {}
-            override fun onStartPreview(previewView: PreviewView?, progress: Int) {}
-            override fun onStopPreview(previewView: PreviewView?, progress: Int) {
-                if (getRealProgress() != pager.currentItem)
-                    pager.setCurrentItem(getRealProgress(), false)
-            }
-        })
-
-    }
-
-    private fun setSeekerProgressDrawable(direction: ReaderViewPager.DIRECTION) {
+    private fun setSeekerProgressDrawable(direction: ReadMode) {
         when (direction) {
-            ReaderViewPager.DIRECTION.LEFT_TO_RIGHT -> {
+            ReadMode.LEFT_TO_RIGHT -> {
                 val ld = getProgressDrawable() as LayerDrawable // lol if this ever changes
                 val d1 = ld.findDrawableByLayerId(android.R.id.background)
                 val d2 = ld.findDrawableByLayerId(android.R.id.progress)
@@ -81,7 +60,7 @@ class ReaderSeekbar @JvmOverloads constructor(context: Context,
                 d2.setColorFilter(ContextCompat.getColor(context, R.color.grey),
                         PorterDuff.Mode.SRC_IN)
             }
-            ReaderViewPager.DIRECTION.RIGHT_TO_LEFT -> {
+            ReadMode.RIGHT_TO_LEFT -> {
                 val ld = getProgressDrawable() as LayerDrawable  // lol if this ever changes
                 val d1 = ld.findDrawableByLayerId(android.R.id.background)
                 val d2 = ld.findDrawableByLayerId(android.R.id.progress)
@@ -95,5 +74,19 @@ class ReaderSeekbar @JvmOverloads constructor(context: Context,
             }
         }
     }
+
+
+    interface OnSeekListener {
+        fun onSeekTo(position: Int)
+    }
+
+    internal class SimplePreviewChangeListener constructor(var onSeekListener: OnSeekListener?) : PreviewView.OnPreviewChangeListener {
+        override fun onPreview(previewView: PreviewView?, progress: Int, fromUser: Boolean) {}
+        override fun onStartPreview(previewView: PreviewView?, progress: Int) {}
+        override fun onStopPreview(previewView: PreviewView?, progress: Int) {
+            onSeekListener?.onSeekTo(progress)
+        }
+    }
+
 
 }
